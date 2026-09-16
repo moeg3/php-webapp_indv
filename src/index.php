0a92625021812bab08b702527cfb857d636a77b2
moeg3 <bb38122105@ms.nagasaki-u.ac.jp>
2026-09-16 11:56:13 +0900

    first commit

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>オリジナルアプリ</title>
+        <link rel="stylesheet" href="m6_style.css">
+    </head>
+    <body>
+        <?php phpinfo() ?>
+    </body>
+</html>
